dd04b7edf3be16cc86d45bc2d7454f9c22915103
ahmedmeskache <ahmedmeskache84@gmail.com>
2026-09-04 18:08:03 +0000

    Rename Aria Abaya to Aria Maxi Dress and add Hijab section

--- js/products.js
+++ js/products.js
@@ -9,6 +9,7 @@ ROSEDRESS.IMG_BY_CAT = {
   "dresses":       "images/cat_dresses.jpg",
   "evening":       "images/cat_evening.jpg",
   "modest":        "images/cat_modest.jpg",
+  "hijab":         "images/dress_ruchedmidi.jpg",
   "accessories":   "images/bag_shoulder.jpg",
   "fragrance":     PX + "264819/pexels-photo-264819.jpeg?auto=compress&cs=tinysrgb&w=900"
 };
@@ -25,7 +26,9 @@ ROSEDRESS.IMG = {
   p9:  "images/sandals_mule1.jpg",
   p10: PX + "32645088/pexels-photo-32645088.jpeg?auto=compress&cs=tinysrgb&w=900",
   p11: PX + "14238877/pexels-photo-14238877.jpeg?auto=compress&cs=tinysrgb&w=900",
-  p12: "images/dress_halterties.jpg"
+  p12: "images/dress_halterties.jpg",
+  p13: "images/dress_halter.jpg",
+  p14: "images/dress_lacestrap.jpg"
 };
 
 ROSEDRESS.CATEGORIES = [
@@ -33,14 +36,15 @@ ROSEDRESS.CATEGORIES = [
   {id:"dresses",      name:"Dresses"},
   {id:"evening",      name:"Evening Collection"},
   {id:"modest",       name:"Modest Collection"},
+  {id:"hijab",        name:"Hijab"},
   {id:"accessories",  name:"Accessories"},
   {id:"fragrance",    name:"Fragrance"}
 ];
 
 ROSEDRESS.PRODUCTS = [
-  {id:"p1", name:"The Aria Abaya",      cat:"modest",      price:8900,  tag:"new", img:"modest"},
+  {id:"p1", name:"Aria Maxi Dress",      cat:"modest",      price:8900,  tag:"new", img:"modest"},
   {id:"p2", name:"Selene Evening Gown", cat:"evening",     price:16400, tag:"",   img:"evening"},
-  {id:"p3", name:"Layla Chiffon Hijab", cat:"modest",      price:2400,  tag:"",   img:"modest"},
+  {id:"p3", name:"Layla Chiffon Hijab", cat:"hijab",       price:2400,  tag:"",   img:"hijab"},
   {id:"p4", name:"Noor Day Dress",      cat:"dresses",     price:7400,  tag:"new", img:"dresses"},
   {id:"p5", name:"Amara Pleated Skirt", cat:"dresses",     price:5200,  tag:"",   img:"dresses"},
   {id:"p6", name:"Zahra Beaded Gown",   cat:"evening",     price:19800, tag:"",   img:"evening"},
@@ -49,7 +53,9 @@ ROSEDRESS.PRODUCTS = [
   {id:"p9", name:"Rima Gold Sandals",   cat:"accessories", price:3900,  tag:"",   img:"accessories"},
   {id:"p10", name:"Rose Élixir Parfum", cat:"fragrance",   price:5800,  tag:"",   img:"fragrance"},
   {id:"p11", name:"Amber Rose Eau",     cat:"fragrance",   price:4600,  tag:"new", img:"fragrance"},
-  {id:"p12", name:"Nadia Embroidered Gown", cat:"evening", price:17900, tag:"",   img:"evening"}
+  {id:"p12", name:"Nadia Embroidered Gown", cat:"evening", price:17900, tag:"",   img:"evening"},
+  {id:"p13", name:"Nadia Silk Hijab",        cat:"hijab",   price:3000,  tag:"new", img:"hijab"},
+  {id:"p14", name:"Sofia Everyday Hijab",    cat:"hijab",   price:2600,  tag:"",   img:"hijab"}
 ];
 
 ROSEDRESS.getCat = function(id){ return ROSEDRESS.CATEGORIES.find(function(c){return c.id===id;}); };
